19c2b4b403f1c1d64af55b83340c2475404d535d
ZestP <shadowevil@126.com>
2019-06-01 11:11:46 +0800

    basic ppm renderer.

--- SimpleRaytracer/SimpleRaytracer.cpp
+++ SimpleRaytracer/SimpleRaytracer.cpp
@@ -3,10 +3,27 @@
 
 #include "pch.h"
 #include <iostream>
-
+#include <fstream>
+using namespace std;
+ofstream fout("test.ppm");
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int nx = 200;
+	int ny = 100;
+	fout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++)
+		{
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99*r);
+			int ig = int(255.99*g);
+			int ib = int(255.99*b);
+			fout << ir << ' ' << ig << ' ' << ib << "\n";
+		}
+	}
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
---
+++ SimpleRaytracer/vec3.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include "vec3.h"
+
+
+vec3::vec3()
+{
+}
+
+inline vec3 & vec3::operator+=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+inline vec3 & vec3::operator-=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+inline vec3 & vec3::operator*=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+inline vec3 & vec3::operator/=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+inline vec3 & vec3::operator*=(const float t)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+inline vec3 & vec3::operator/=(const float t)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+
+inline void vec3::make_unit_vector()
+{
+}
+
+vec3::~vec3()
+{
+}
